aa596bc01983c5536e206f2fb510b851c93491c0
Marcos R Oliveira <mroger.oliveira@gmail.com>
2014-08-17 17:58:18 -0300

    Changes to support library enhancements.

--- examples/SimpleLibDemo/SimpleLibDemo.ino
+++ examples/SimpleLibDemo/SimpleLibDemo.ino
@@ -4,11 +4,16 @@
 
 TEA5767N radio = TEA5767N();
 
+/*
+  This code shows some of the uses of the TEA5767N API.
+*/
+
 void setup() {
+  byte isBandLimitReached;
 
   //Sets the radio station
   radio.selectFrequency(89.1);
-  delay(2000);
+  delay(5000);
   
   radio.mute();
   delay(2000);
@@ -31,36 +36,60 @@ void setup() {
   //Turns the radio off
   radio.setStandByOn();
   delay(2000);
+  //Turns the radio on
   radio.setStandByOff();
   
-  radio.selectFrequency(107.9);
-  radio.setSearchUp();
+  Serial.println("Search Up in progress...");
   radio.setSearchMidStopLevel();
-  radio.searchNext();
-  while(!radio.isReady()) {
-    Serial.println("Search in progress...");
-  }
-  if (radio.isBandLimitReached()) {
-     Serial.println("Band Limit Reached, searching down...");
-     radio.setSearchDown();
-     radio.searchNext();
-  }
+  isBandLimitReached = radio.startsSearchMutingFromBeginning();
+  
+  float freq = radio.readFrequencyInMHz();
+  Serial.println(freq, 1);
   delay(2000);
   
-  //Reading the status
+  while (!isBandLimitReached) {
+    
+    Serial.println("Search Up in progress...");
+    //If you want listen to station search, use radio.searchNext() instead
+    isBandLimitReached = radio.searchNextMuting();
+    
+    freq = radio.readFrequencyInMHz();
+    Serial.println(freq, 1);
+    delay(2000);
+  }
+  
+  Serial.println("Band Limit Reached, searching down...");
+  
+  radio.setSearchDown();
+  radio.setSearchMidStopLevel();
+  
+  Serial.println("Search Down in progress...");
+  isBandLimitReached = radio.searchNextMuting();
+  
+  freq = radio.readFrequencyInMHz();
+  Serial.println(freq, 1);
+  delay(2000);
+  
+  while (!isBandLimitReached) {
+    
+    Serial.println("Search Down in progress...");
+    isBandLimitReached = radio.searchNextMuting();
+        
+    freq = radio.readFrequencyInMHz();
+    Serial.println(freq, 1);
+    delay(2000);
+  }
+  
+  radio.selectFrequency(89.1);
+
   //Read frequenci in MHz
   Serial.println(radio.readFrequencyInMHz(), 1);
   //Shows reception signal level
   Serial.println(radio.getSignalLevel());
   //Reception stereo or mono?
   Serial.println(radio.isStereo() ? "Stereo" : "Mono");
-  //Ready for new instructions? Done with frequency selection and search?
-  Serial.println(radio.isReady() ? "Ready" : "Not ready");
-  //Has reached superior or inferior band limit?
-  Serial.println(radio.isBandLimitReached() ? "Limit reached" : "Limit not reached");
 }
  
 void loop() {
   
 }
-
