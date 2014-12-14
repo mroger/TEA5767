58cab5244c7c33d21f03763995234ce8382301f6
Marcos R Oliveira <mroger.oliveira@gmail.com>
2014-12-14 21:46:50 -0200

    Making copy more explicit

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,2 @@
-copy TEA5767N.* "C:\Documents and Settings\Roger\Meus documentos\Arduino\libraries\TEA5767N\"
+copy TEA5767N.cpp "C:\Documents and Settings\Roger\Meus documentos\Arduino\libraries\TEA5767N\"
+copy TEA5767N.h "C:\Documents and Settings\Roger\Meus documentos\Arduino\libraries\TEA5767N\"
